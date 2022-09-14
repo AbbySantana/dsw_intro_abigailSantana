c63dd681b6002013858be0c4473c8f40cd25e2c4
AbbySantana <96422865+AbbyChuelita@users.noreply.github.com>
2022-09-14 12:20:43 +0100

    Create tabla.php

---
+++ tabla.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<table class="default">
+
+<tr>
+
+  <td><?php echo date("d-m-Y")?></td>
+
+
+</tr>
+
+<tr>
+
+  <td><?php echo date("h:i:s")?></td>
+
+</tr>
+
+</table>
+
+
+</body>
+</html>
