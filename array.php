a7b0ed4e9f1603fad8b8e369e99380c5edd09605
AbbySantana <mariaabigailsantanarodriguez@alumno.ieselrincon.es>
2022-09-19 13:38:47 +0100

    Añadido todos los dias de la semana

--- array.php
+++ array.php
@@ -1,8 +1,12 @@
 <?php 
 $array = array(
     1287319812 => array("correo"=>"Felix Santiago", "correo"=>"felixsantiago@gmail.com", "telefono"=>"927928350"),
-    2387429492 => "Lucas Ojeda");
+    2387429492 => array("Lucas Ojeda");
 
 echo "<pre>";
+
 print_r($array);
 echo "<pre>";
+
+foreach($array as $clave)
+
--- mariaAbigailSantanaRodriguez-queTengoAhora.php
+++ mariaAbigailSantanaRodriguez-queTengoAhora.php
@@ -1,23 +1,51 @@
 <?php
 
 $horario = [
-    "Lunes" => array("Primera" => "EMR", "Docente" => "Maria del Sol García Tarajano", "Aula" => "G201");
-                    array("Segunda" => "DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201");
-                        array("Tercera"=>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201");
-                            array("Cuarta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201");
-                                array("Quinta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201");
-                                    array("Sexta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201");
+    "Lunes" => [array("Primera" => "EMR", "Docente" => "Maria del Sol García Tarajano", "Aula" => "G201"),
+                array("Segunda" => "DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Tercera"=>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Cuarta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Quinta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Sexta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201")],
+                 "<br>",
 
-    
-]
+    "Martes" => [array("Primera" => "DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Segunda" => "DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Tercera"=>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Cuarta" =>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Quinta" => "DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201"),
+                array("Sexta" =>"DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201")], 
+                "<br>", 
 
-function queTocaAhora($dia, $hora, $minutos)
-{
-    foreach($horario as $clave=> $valor){
-        if($dia=="Lunes")
-            if($hora==8 && $minutos<=55)   
-                foreach($horario as $clave1 => $valor1) 
-                 echo "Ahora toca $valor1";
-    }
+    "Miércoles" => [array("Primera" =>"DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Segunda" =>"DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Tercera"=>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Cuarta" =>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Quinta" => "DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201"),
+                array("Sexta" =>"DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201")],      
+                "<br>",
 
-}
+    "Jueves" => [array("Primera" =>"DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Segunda" =>"DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Tercera"=>"DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Cuarta" =>"DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Quinta" => "DEW", "Docente" => "Maria del Carmen Rodríguez Suárez", "Aula" => "G201"),
+                array("Sexta" =>"EMR", "Docente" => "Maria del Sol García Tarajano", "Aula" => "G201")],  
+                "<br>",
+                     
+
+    "Viernes" => [array("Primera" =>"DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201"),
+                array("Segunda" =>"DOR", "Docente" => "Ermís Papakinstantinouu Báez", "Aula" => "G201"),
+                array("Tercera"=>"DPL", "Docente" => "María Antonia Montesdeoca Viera", "Aula" => "G201"),
+                array("Cuarta" => "EMR", "Docente" => "Maria del Sol García Tarajano", "Aula" => "G201"),
+                array("Quinta" =>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"),
+                array("Sexta" =>"DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201")]    
+
+
+];
+
+
+print_r($horario);
+
+
+
