62826f3d8b234e940d940e0d3685ce445e33a271
AbbySantana <mariaabigailsantanarodriguez@alumno.ieselrincon.es>
2022-09-21 12:13:30 +0100

    hora minutos realizado

--- mariaAbigailSantanaRodriguez-queTengoAhora.php
+++ mariaAbigailSantanaRodriguez-queTengoAhora.php
@@ -1,5 +1,7 @@
 <?php
 //Elaboración de una tabla para el horario
+
+
 $horario = [
     "Lunes" => ["Primera" => ["EMR", "Docente" => "Maria del Sol García Tarajano", "Aula" => "G201"],
                 "Segunda" => ["DSW", "Docente" => "Sergio Ramos Suarez", "Aula" => "G201"],
@@ -60,19 +62,63 @@ foreach($horario as $clave=> $valor){
 
 
 
+//Función para que me devuelva el orden
+
+function ordenClase($hora, $minutos){
+
+    $horaTotal=($hora*100) + $minutos;
+
+    if($horaTotal>=800 && $horaTotal<=854){
+        return "Primera";
+    }
+
+    if($horaTotal>=855 && $horaTotal<=949){
+        return "Segunda";
+    }
+
+    if($horaTotal>=950 && $horaTotal<=1044){
+        return "Tercera";
+    }
+
+    if($horaTotal>=1045 && $horaTotal<=1114){
+        return "Recreo";
+    }
+
+    if($horaTotal>=1115 && $horaTotal<=1209){
+        return "Cuarta";
+    }
+
+    if($horaTotal>=1210 && $horaTotal<=1304){
+        return "Quinta";
+    }
+
+    if($horaTotal>=1305 && $horaTotal<=1400){
+        return "Sexta";
+    }
+    
+}
 
 // Función para asociar una hora con la asignatura.
 
 function queTocaAhora($dia, $hora, $minutos){
+    global $horario;
+    $horaTotal=($hora*100) + $minutos;
+    
+    if ($horaTotal<=759 || $horaTotal>=1401){
+        echo "No hay clases";
+    }
 
+    $orden = ordenClase($hora, $minutos);
    
+    
+    return $horario[$dia][$orden];
+    
+           
 
 }
 
+echo "<br><br>";
 
 
-
-
-
-
-
+$ahora = queTocaAhora("Lunes",8,01);
+echo "Ahora se imparte " .$ahora[0];
